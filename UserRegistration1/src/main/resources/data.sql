4529bb0c119bca76724de0333830e60f4d17ea18
aadeeshjain <43640059+aadeeshjain@users.noreply.github.com>
2020-05-08 17:07:47 +0530

    Update data.sql

--- UserRegistration1/src/main/resources/data.sql
+++ UserRegistration1/src/main/resources/data.sql
@@ -1,2 +1,4 @@
 insert into User (user_id,user_fname,user_lname,user_email,user_pass,user_mobile) values (1,'Aadeesh','Jain','aadeeshjain.91a@yahoo.com','abcd@234','9876543212');
 insert into User (user_id,user_fname,user_lname,user_email,user_pass,user_mobile) values (2,'Andrew','Jate','andrewjate.df@yahoo.com','andrew@9','9876547542');
+insert into User (user_id,user_fname,user_lname,user_email,user_pass,user_mobile) values (3,'John','Deed','john619.91a@yahoo.com','john@3113','9876544444');
+insert into User (user_id,user_fname,user_lname,user_email,user_pass,user_mobile) values (4,'Peter','Parker','peter.parker@gmail.com','pet_er@9','9871117542');
